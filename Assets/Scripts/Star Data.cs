79bf514f19dc03194ed51c3e91aa3cbd198c8034
Sydney <sydneylai77@gmail.com>
2026-03-15 16:17:02 -0700

    I made so many changes I am going to EXPLODE!!

--- Assets/Scripts/Star Data.cs
+++ Assets/Scripts/Star Data.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 
-public class StarData
+public class StarData : MonoBehaviour
 {
-    public string starName;
+    [Header("Star Identity")]
+    public int starID;            // 0=Curious, 1=Happy, 2=Lazy, 3=Mischevious, 4=Shy
+    public GameObject starPrefab; // The prefab to shoot out
+    public Color staffColor;      // The color the staff turns
+    // public string starName;
     public Color starColor;
-    public GameObject starPrefab;
+    //public GameObject starPrefab;
 }
